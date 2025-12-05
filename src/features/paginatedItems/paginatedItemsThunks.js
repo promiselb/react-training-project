c6713274c13193a53fd050b0ca79d705bb026ab5
promiselb <waedmansour1222006@gmail.com>
2025-12-05 16:11:08 +0200

    The Final Commit

--- src/features/paginatedItems/paginatedItemsThunks.js
+++ src/features/paginatedItems/paginatedItemsThunks.js
@@ -7,8 +7,7 @@ export const fetchPaginatedItems = createAsyncThunk(
     try {
         const res = await fetch(`/api/items?_page=${page}&_per_page=${perPage}`);
         const data = await res.json(); 
-        // const totalCount = res.headers.get("X-Total-Count"); // JSON server pagination header
-        const totalCount = NUMBER_OF_ITEMS; // Temporary fix for total count
+        const totalCount = NUMBER_OF_ITEMS; 
         const totalPages = Math.ceil(totalCount / perPage) || 6;
         const reurnvalue = { 
             items: data.data,
